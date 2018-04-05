876b0d570fd71195ac12e8b3ff0b214398ec454c
kvillanueva000 <karina60804@yahoo.com>
2018-04-05 03:35:38 -0500

    Implemented JFrame.

--- OvalDraw.java
+++ OvalDraw.java
@@ -1,5 +1,13 @@
+import javax.swing.JFrame;
+
 public class OvalDraw {
     public static void main(String[] args) {
         System.out.println("OvalDraw Starting...");
+
+        JFrame myFrame = new JFrame();
+        myFrame.setTitle("OvalDraw");
+        myFrame.setBounds(200,200,300,400);
+        myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
