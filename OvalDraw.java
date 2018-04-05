c901d18c00d84d19fce8edabd02c598e1a451ef1
kvillanueva000 <karina60804@yahoo.com>
2018-04-05 04:09:09 -0500

    Implemented blue oval.

--- OvalDraw.java
+++ OvalDraw.java
@@ -1,4 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Container;
+import java.awt.Color;
+
+class Oval extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        g.setColor(new Color(0,0,255));
+        g.fillOval(0,0,panelWidth,panelHeight);
+    }
+}
 
 public class OvalDraw {
     public static void main(String[] args) {
@@ -9,5 +25,9 @@ public class OvalDraw {
         myFrame.setBounds(200,200,300,400);
         myFrame.setVisible(true);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Oval myOval = new Oval();
+        Container contentPane = myFrame.getContentPane();
+        contentPane.add(myOval);
     }
 }
